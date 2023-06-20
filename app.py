6184079932c61d527511b9c4a2e2aa98805a50d1
Mwlod Ghareeb <mwlodghareeb@Mwlods-MacBook-Air.local>
2023-06-20 10:53:41 -0400

    Adding project files

---
+++ app.py
@@ -0,0 +1,28 @@
+import json
+import pickle
+
+from flask import Flask,request , render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+## Load the model
+model=pickle.load(open('model_final.pkl','rb'))
+scalar=pickle.load(open('stand_scaler.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=round(np.exp(model.predict(final_input)[0]),2)
+    return render_template("home.html",prediction_text="The Estimated medical cost is {}".format(output))
+
+
+
+if __name__=="__main__":
+     app.run(host="0.0.0.0")
